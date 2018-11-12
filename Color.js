369c9737b66756016f6e90168e3e42657f5fc7bc
JURIC-PC\JURIC <vlado.juric35@gmail.com>
2018-11-12 19:00:58 +0100

    new component
    create new component

---
+++ Color.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Color extends Component {
+    render() {
+      return (
+        <p>
+            radi
+        </p>
+      );
+    }
+  }
+  
+  export default Color;
