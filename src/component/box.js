bdfa15818d7145f5d42f11bc07af06d38a7135cd
JURIC-PC\JURIC <vlado.juric35@gmail.com>
2018-11-12 20:35:26 +0100

    new component
    add new component to project plus button

---
+++ src/component/box.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Box extends React.Component{
+    render(){
+        return(
+            <div>
+                <button>
+                    Change Color
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Box;
