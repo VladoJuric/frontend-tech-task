925d81c0bd99ce5e4ec14cdba90543d0e20a51a9
JURIC-PC\JURIC <vlado.juric35@gmail.com>
2018-11-12 19:03:26 +0100

    new component test
    Test new component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Color from './Color.js';
 
 class App extends Component {
   render() {
@@ -10,6 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Test
+            <Color>
+
+            </Color>
           </p>
           <a
             className="App-link"
