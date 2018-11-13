ef0291daeb3d64236af34edada39091b341a5b6d
JURIC-PC\JURIC <vlado.juric35@gmail.com>
2018-11-13 17:34:32 +0100

    commit random two colors
    Working with two collors but error on color2

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,19 @@ import axios from "axios";
 export function loadColor(){
     return(dispatch) => {
         return axios.get("http://www.colr.org/json/color/random").then((response)=>{
-            alert(dispatch(changeColor("#"+response.data.new_color)))
-            alert('API')
+            let color1 = "#" + response.data.new_color;
+            let color2 = "#" + response.data.colors[0].tags[0].name;
+            let color;
+            console.log(color1 + " - " + color2)
+            let rand = Math.random() * 1
+            console.log(rand)
+            if (rand >= 0.5) {
+                color = color1
+            } else {
+                color = color2
+            }
+
+            dispatch(changeColor(color))
         })
     }
 }
