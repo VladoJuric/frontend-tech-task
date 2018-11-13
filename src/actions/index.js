ba8ac46073da9315c51086a5f34d77a4570c00b5
JURIC-PC\JURIC <vlado.juric35@gmail.com>
2018-11-13 18:30:31 +0100

    commit mixColor
    working on mixColor function

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,23 +4,38 @@ export function loadColor(){
     return(dispatch) => {
         return axios.get("http://www.colr.org/json/color/random").then((response)=>{
             let color = [];    
-        
-            color[0] = "black"
-            color[1] = "#" + response.data.new_color;
-            color[2] = "#" + response.data.colors[0].tags[0].name;
 
-            console.log(color[0] + " - " + color[1] + " - " + color[3])
+            color[0] = "#" + response.data.new_color;
+            color[1] = response.data.colors[0].tags[0].name;
+            color[2] = "#000000"
 
-            dispatch(changeColor(color))
+            let result = mixColor(color);
+            console.log(result)
+
+            dispatch(changeColor(result))
         })
     }
 }
 
-export function changeColor(color){
-    let rand = Math.floor(Math.random() * 3)
+function mixColor(color){
+    let result;
+    let rand = Math.floor(Math.random() * 2)
     console.log(rand)
+    console.log(color[rand] + " - " + color[2])
+
+    result = "#" + color[rand].substring(1,2)
+                 + color[2].substring(2,3)
+                 + color[rand].substring(3,4)
+                 + color[2].substring(4,5)
+                 + color[rand].substring(5,6)
+                 + color[2].substring(6,7)
+    return{
+        result
+    }
+}
+export function changeColor(color){
     return{
         type:"CHANGE_COLOR",
-        color:color[rand]
+        color:color
     }
 }
